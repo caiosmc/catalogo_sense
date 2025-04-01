c6753e35cd041b828a3f0cf83559134f4c43d5cf
caiosmc <caio@carrocino.com>
2025-04-01 17:41:14 -0300

    Update urlFiltros.js

--- src/utils/urlFiltros.js
+++ src/utils/urlFiltros.js
@@ -1,25 +1,34 @@
-export function obterCategoriasDaURL() {
+// src/utils/urlFiltros.js
+
+export const obterCategoriasDaURL = () => {
   const params = new URLSearchParams(window.location.search);
   const categorias = params.get("categorias");
   return categorias ? categorias.split(",") : [];
-}
+};
 
-export function obterBuscaDaURL() {
+export const atualizarURLComCategorias = (categorias) => {
   const params = new URLSearchParams(window.location.search);
-  return params.get("busca") || "";
-}
-
-export function atualizarParametrosURL(categorias, busca) {
-  const params = new URLSearchParams();
-
   if (categorias.length > 0) {
     params.set("categorias", categorias.join(","));
+  } else {
+    params.delete("categorias");
   }
-
-  if (busca) {
-    params.set("busca", busca);
-  }
-
   const novaURL = `${window.location.pathname}?${params.toString()}`;
   window.history.replaceState(null, "", novaURL);
-}
+};
+
+export const obterTermoBuscaDaURL = () => {
+  const params = new URLSearchParams(window.location.search);
+  return params.get("busca") || "";
+};
+
+export const atualizarURLComBusca = (busca) => {
+  const params = new URLSearchParams(window.location.search);
+  if (busca) {
+    params.set("busca", busca);
+  } else {
+    params.delete("busca");
+  }
+  const novaURL = `${window.location.pathname}?${params.toString()}`;
+  window.history.replaceState(null, "", novaURL);
+};
